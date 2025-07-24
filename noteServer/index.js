9c117dca21b9b60168671123dc226f2a5c69bf59
wasp <wasp123321@gmail.com>
2025-07-24 18:11:38 +0200

    noteServer added static frontend

--- noteServer/index.js
+++ noteServer/index.js
@@ -4,7 +4,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -14,13 +13,15 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(requestLogger)
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+app.use(express.static('dist'))
+app.use(cors())
 app.use(express.json())
+app.use(requestLogger)
 
 
 let notes =  [
