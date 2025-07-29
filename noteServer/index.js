ff022ca7aa9d408222b8092a24cf3552cf7f1f5f
wasp <wasp123321@gmail.com>
2025-07-29 18:00:52 +0200

    Added errorHandler, PUT and DELETE

--- noteServer/index.js
+++ noteServer/index.js
@@ -18,29 +18,20 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name = 'CastError') {
+    return response.status(400).send({ error: 'Malformed ID'})
+  }
+
+  next(error)
+}
+
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
-
-
-let notes =  [
-    {
-      "id": "1",
-      "content": "HTML is easy",
-      "important": true
-    },
-    {
-      "id": "2",
-      "content": "Browser can execute only JavaScript",
-      "important": false
-    },
-    {
-      "id": "3",
-      "content": "GET and POST are the most important methods of HTTP protocol",
-      "important": true
-    }
-  ]
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World</h1>')
@@ -52,10 +43,16 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
   })
+  .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -77,15 +74,39 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(n => n.id !== id)
+app.delete('/api/notes/:id', (request, response, next) => {
+  // const id = request.params.id
+  // notes = notes.filter(n => n.id !== id)
 
-  response.status(204).end()
+  // response.status(204).end()
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if(!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then(updatedNote => {
+        response.json(updatedNote)
+      })
+    })
+    .catch (error => next(error))
 })
 
 app.use(unknownEndpoint)
-
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
