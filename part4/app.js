49b7dadea4e92f6baaf7c5df1811619475de8cfe
wasp <wasp123321@gmail.com>
2025-08-03 16:07:40 +0200

    Part4 backend setup

---
+++ part4/app.js
@@ -0,0 +1,27 @@
+
+const express = require('express')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+const app = express()
+
+
+logger.info('connecting to MongoDB')
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB', error.message)
+  })
+
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+
+
+module.exports = app
