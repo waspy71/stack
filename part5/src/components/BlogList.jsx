d1995256d23c95cccafd975b9ea06a365d397c1b
wasp <wasp123321@gmail.com>
2025-09-06 16:48:41 +0200

    Exercise 5.10 complete

--- part5/src/components/BlogList.jsx
+++ part5/src/components/BlogList.jsx
@@ -6,7 +6,7 @@ const BlogList = ({ blogs, handleLikes }) => {
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {blogs.toSorted((a, b) => b.likes - a.likes).map(blog =>
         <Blog
           key={blog.id}
           blog={blog}
