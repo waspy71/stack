df2d9a9e163995d3ae8cdaf316e37d90fbdf59c0
wasp <wasp123321@gmail.com>
2025-09-19 16:49:16 +0200

    Created helper functions

--- noteApp-Playwrigth/playwright.config.js
+++ noteApp-Playwrigth/playwright.config.js
@@ -24,11 +24,11 @@ export default defineConfig({
   workers: 1, // was -->> process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  timeout: 3001,
+  timeout: 6001,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
--- noteApp-Playwrigth/tests/note_app.spec.js
+++ noteApp-Playwrigth/tests/note_app.spec.js
@@ -1,11 +1,22 @@
 
 const { test, expect, describe, beforeEach } = require('@playwright/test')
+const { loginWith, createNote } = require('./helper')
 
-const baseUrl = 'http://localhost:5173'
+
+const user = {
+        name: 'testName',
+        username: 'testUsername',
+        password: 'testPassword'
+      }
 
 describe('Note app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto(baseUrl)
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: user
+    })
+
+    await page.goto('/')
   })
 
   test('front page can be opened', async ({ page }) => {
@@ -15,26 +26,41 @@ describe('Note app', () => {
   })
 
   test('user can log in', async ({ page }) => {
-    await page.getByRole('button', { name: 'login' }).click()
-    await page.getByLabel('username').fill('test')
-    await page.getByLabel('password').fill('test')
-    await page.getByRole('button', { name: 'login' }).click()
-    await expect(page.getByText('test logged in')).toBeVisible()
+    await loginWith(page, user.username, user.password)
+    await expect(page.getByText('testName logged in')).toBeVisible()
+  })
+
+  test('login fails with wrong password', async ({ page }) => {
+    await loginWith(page, user.username, 'wrong')
+    await expect(page.getByText('wrong credentials')).toBeVisible()
+
+    const errorDiv = page.locator('.error')
+    await expect(errorDiv).toContainText('wrong credentials')
+    await expect(errorDiv).toHaveCSS('border-style', 'solid')
+    await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+
+    await expect(page.getByText('testName logged in')).not.toBeVisible()
   })
 
   describe('when logged in', () => {
     beforeEach(async ({ page }) => {
-      await page.getByRole('button', { name: 'login' }).click()
-      await page.getByLabel('username').fill('test')
-      await page.getByLabel('password').fill('test')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, user.username, user.password)
     })
 
     test('a new note can be created', async ({ page }) => {
-      await page.getByRole('button', { name: 'new note' }).click()
-      await page.getByRole('textbox').fill('a note created by playwright')
-      await page.getByRole('button', { name: 'save' }).click()
+      await createNote(page, 'a note created by playwright')
       await expect(page.getByText('a note created by playwright')).toBeVisible()
+    })
+
+    describe('and a note exist', () => {
+      beforeEach(async ({ page }) => {
+        await createNote(page, 'a note created by playwright')
+      })
+
+      test('importance can be changed', async ({ page }) => {
+        await page.getByRole('button', { name: 'make not important' }).click()
+        await expect(page.getByText('make important')).toBeVisible()
+      })
     })
   })
 })
