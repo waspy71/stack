3de2d7c4c02468ce49e20f3dcf8c1080379c7c98
wasp <wasp123321@gmail.com>
2025-10-12 17:19:35 +0200

    Exercise 6.20 complete

--- part6/query-anecdotes-main/src/App.jsx
+++ part6/query-anecdotes-main/src/App.jsx
@@ -1,19 +1,35 @@
+
+import { useQuery } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAll } from './services/anecdotes'
 
 const App = () => {
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAll,
+    retry: 1
+  })
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  // const anecdotes = [
+  //   {
+  //     "content": "If it hurts, do it more often",
+  //     "id": "47145",
+  //     "votes": 0
+  //   },
+  // ]
+
+  if(result.isLoading) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
+
 
   return (
     <div>
