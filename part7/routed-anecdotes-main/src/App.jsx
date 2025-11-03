97ffdbdec9f2157e4a3e3eea9f4b1e61d9f115c1
wasp <wasp123321@gmail.com>
2025-11-03 12:07:50 +0100

    Added Bootstrap styles to Navbar

--- part7/routed-anecdotes-main/src/App.jsx
+++ part7/routed-anecdotes-main/src/App.jsx
@@ -1,17 +1,33 @@
 import { useState } from 'react'
 import { Routes, Route, Link, useMatch, useNavigate } from 'react-router-dom'
 import { useField } from './hooks'
+import { Navbar, Nav } from 'react-bootstrap' 
 
 const Menu = () => {
   const padding = {
-    paddingRight: 5
+    paddingRight: 5,
+    color: 'white',
+    textDecoration: 'none',
+    margin: '0px 15px'
   }
+
   return (
-    <div>
-      <Link style={padding} to='/'>anecdotes</Link>
-      <Link style={padding} to='/create'>create new</Link>
-      <Link style={padding} to='/about'>about</Link>
-    </div>
+    <Navbar collapseOnSelect className='rounded' expand="lg" bg="info" variant="dark" >
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className='me-auto'>
+          <Nav.Link href="#" as="span">
+            <Link style={padding} to='/'>anecdotes</Link>
+          </Nav.Link>
+          <Nav.Link href="#" as="span">
+            <Link style={padding} to='/create'>create new</Link>
+          </Nav.Link>
+          <Nav.Link href="#" as="span">
+            <Link style={padding} to='/about'>about</Link>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
@@ -175,7 +191,7 @@ const App = () => {
     : null
 
   return (
-    <div>
+    <div className='container'>
       <h1>Software anecdotes</h1>
       <Menu />
       <Notification notification={notification} />
